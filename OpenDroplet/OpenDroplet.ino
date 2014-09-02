b648f6a8a6db6c7e5722b0ab08eb3f74b57d25f9
Alex Shure <as+github@etemu.com>
2014-09-02 17:37:22 +0200

    starting on visual feedback
    with WS2812b RGB LEDs

--- OpenDroplet/OpenDroplet.ino
+++ OpenDroplet/OpenDroplet.ino
@@ -11,26 +11,58 @@
  CC0
  
  */
+ 
+#include <Adafruit_NeoPixel.h>
 
-const unsigned int micPin = A0;    // Analog input pin where the mic is attached to
-const unsigned int outPin = 13;    // Analog output pin, LED for testing purposes
+#define LEDPIN 2
+#define LEDPIXEL 1
+#define MICPIN A0    // Analog input pin where the mic is attached to
+#define OUTPIN 13    // Analog output pin, LED for testing purposes
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel neoled = Adafruit_NeoPixel(LEDPIXEL, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 int mic = 0;              // value read from the mic
 int lastmic = 0;          // previous reading from the mic
 int out = 0;              // value output to the PWM (analog out)
-
+int showerState = 0;
 void setup() {
   // initialize serial communications at 115200 bps:
   Serial.begin(115200);
+  pinMode(LEDPIN,OUTPUT);
+  pinMode(MICPIN,INPUT);
+  pinMode(OUTPIN,OUTPUT);
+
+
+  ledoff();
+}
+
+void led(uint8_t _mode=0){
+  uint16_t r,g,b = 0;
+  for(uint16_t l = 0; l < LEDPIXEL; l++) {
+      neoled.setPixelColor(l, 0, 0, 0);
+    }
+}
+
+void ledoff(){
+    for(uint16_t l = 0; l < LEDPIXEL; l++) {
+      neoled.setPixelColor(l, 0, 0, 0);
+    }
 }
 
 void loop() {
   // read the mic volume:
-  mic = analogRead(micPin);
+  mic = analogRead(MICPIN);
   // map it to the range of the analog out:
   out = map(mic, 0, 1023, 255, 0);
   // change the analog out value:
-  analogWrite(outPin, out);
+  analogWrite(OUTPIN, out);
   if (lastmic+10 < mic) { showerState=1; }
 
   // print the results to the serial monitor:
@@ -42,5 +74,5 @@ void loop() {
   // wait before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(4);
+  delay(20);
 }
