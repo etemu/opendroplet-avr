97855543234adf5f8ab8fa467b89f69aca7bf729
Alex Shure <as+github@etemu.com>
2014-10-27 23:00:44 +0100

    WIP save config in EEPROM

---
+++ OpenDroplet-configurator/NodeEepromWrite/NodeEepromWrite.ino
@@ -0,0 +1,38 @@
+/*
+ * 
+ * Stores configuration in the EEPROM.
+ * These values will stay in the EEPROM when the board is
+ * turned off and may be retrieved later when booting the node.
+ */
+
+#include <EEPROM.h>
+
+// Change these values according to your network setup:
+static byte nodeID = 7; // range: 2-254
+static byte UID[] = { 255, 242, 255, 255, 255, nodeID }; // choose a UID
+static byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, nodeID }; // choose a unique MAC (optional, only if ethernet used)
+static byte ip[] = { 192, 168, 8, nodeID }; // your subnet IP scheme (optional, only if ethernet used)
+static byte gw[] = { 192, 168, 8, 1}; // your gateway (optional, only if ethernet used)
+
+
+byte val[] = {nodeID, uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], mac[0],mac[1],mac[2],mac[3],mac[4],mac[5],ip[0],ip[1],ip[2],ip[3],gw[0],gw[1],gw[2],gw[3]};
+
+int addr=0;
+void setup()
+{
+  SaveData();
+}
+
+void loop()
+{
+  delay(1337);
+  
+}
+
+void SaveData(){
+  while (addr < 21){
+  EEPROM.write(addr, val[addr]);
+  // advance to the next address. 
+  addr++;
+    }
+}
--- OpenDroplet/OpenDroplet.ino
+++ OpenDroplet/OpenDroplet.ino
@@ -2,37 +2,31 @@
 
  Open Droplet alpha r&d
  
- iilab ltd.
- iilab.org
- 
  shure open technologies
  etemu.com
-  
+ iilab.org  
  CC0
  
  */
+
+#include <Adafruit_NeoPixel.h>	// NeoPixel lib for visual feedback with RGB LEDs when debugging
  
-#include <Adafruit_NeoPixel.h>
+#define DEBUG 1 				// debug mode with verbose output over serial at 115200 bps
+#define USE_EEPROM 				// read nodeID and network settings from EEPROM at bootup
+#define KEEPALIVE_RATE 32768L 	// request rate of sendKeepalive in ms
+#define LEDPIN 2     			// PortPin where the LED pixels are attached/daisy chained
+#define LEDPIXEL 1   			// Number of LED pixels attached
+#define MICPIN A0    			// Analog input pin where the mic is attached to
+#define OUTPIN 13    			// Analog output pin, LED for testing purposes
 
-#define LEDPIN 2     // PortPin where the LED pixels are attached/daisy chained
-#define LEDPIXEL 1   // Number of LED pixels attached
-#define MICPIN A0    // Analog input pin where the mic is attached to
-#define OUTPIN 13    // Analog output pin, LED for testing purposes
-
-// Parameters: number of LEDs attached, pin number, pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDPIXEL, LEDPIN, NEO_GRB + NEO_KHZ800);
 
-int mic = 0;              // value read from the mic
-int lastmic = 0;          // previous reading from the mic
-int out = 0;              // value output to the PWM (analog out)
+int mic = 0;              		// value read from the mic
+int lastmic = 0;          		// previous reading from the mic
+int out = 0;              		// value output to the PWM (analog out)
 int showerState = 0;
 void setup() {
-  // initialize serial communications at 115200 bps:
-  Serial.begin(115200);
+  Serial.begin(115200);			// initialize serial communications at 115200 bps:
   pinMode(LEDPIN,OUTPUT);
   pinMode(MICPIN,INPUT);
   pinMode(OUTPIN,OUTPUT);
@@ -57,16 +51,12 @@ void ledoff(){
 }
 
 void loop() {
-  // read the mic volume:
-  mic = analogRead(MICPIN);
-  // map it to the range of the analog out:
-  out = map(mic, 0, 1023, 255, 0);
-  // change the analog out value:
-  analogWrite(OUTPIN, out);
+  
+  mic = analogRead(MICPIN); 			// read the mic volume
+  out = map(mic, 0, 1023, 255, 0); 		// map it to the range of the analog out
+  analogWrite(OUTPIN, out);				// change the analog out value
   if (lastmic+10 < mic) { showerState=1; }
-
-  // print the results to the serial monitor:
-  Serial.print("mic: " );
+  Serial.print("mic: " );				// print the results to the serial monitor:
   Serial.print(mic);
   Serial.print("\t out: ");
   Serial.println(out);
